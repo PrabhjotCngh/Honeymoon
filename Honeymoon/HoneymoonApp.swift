91102aedc46fc46425613262b5a1ae9a6d9626b5
Prabjot Singh <prabjot.singh@andersencorp.com>
2023-06-05 10:27:58 -0400

    Initial project setup

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by M_2195552 on 2023-06-05.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
